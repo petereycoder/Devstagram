c8bac02b62be5acba20a70ff0674b615edff2214
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-13 14:30:23 -0600

    Se agregaron secciones y estilos en el layout principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,29 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     </head>
-    <body>
-        <h1 class="text-3xl font-bold">
-            Hello world!
-          </h1>
+    <body class="bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">
+                    Devstagram
+                </h1>
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear cuenta</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10"> 
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            Devstagram - Todos los derechos reservados 
+            {{now()->year}}
+        </footer>
     </body>
 </html>
