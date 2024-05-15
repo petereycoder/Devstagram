159bd02732510693e5d300593348481fee72fc24
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-15 10:39:01 -0600

    Se agregaron validaciones para que no se repita el usuario y el email

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,6 +16,9 @@ class RegisterController extends Controller
 
     public function store(Request $request) 
     {
+
+      $request->request->add(['username' => Str::slug($request->username)]);
+
       $this->validate($request, [
         'name' => 'required|max:30',
         'username' => 'required|unique:users|min:3|max:20',
