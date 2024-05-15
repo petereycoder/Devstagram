acfb7dca775ae369fe1f343977bc124a32188e9d
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-15 09:26:12 -0600

    Se agregó username en el modelo users

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,10 +15,10 @@ class RegisterController extends Controller
     public function store(Request $request) 
     {
       $this->validate($request, [
-        'name' => 'required | max:30',
-        'username' => 'required | unique:users | min:3 | max:20',
-        'email' => 'required | unique:users | email | max:60',
-        'password' => 'required | confirmed | min:8',
+        'name' => 'required|max:30',
+        'username' => 'required|unique:users|min:3|max:20',
+        'email' => 'required|unique:users|email|max:60',
+        'password' => 'required|confirmed|min:8',
       ]);
 
       User::create([
