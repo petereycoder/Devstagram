e885882cc65dfad9b85160d646970900dd6890cb
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-15 10:51:59 -0600

    Se crearon controladores para derigir el usuario al muro y el login

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -32,5 +32,7 @@ class RegisterController extends Controller
         'email' => $request->email,
         'password' => Hash::make($request->password)
       ]);
+
+      return redirect()->route('posts.index');
     }
 }
