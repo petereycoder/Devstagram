8daaeb39ab531df86fec49c17e64954512f91820
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-16 12:11:18 -0600

    Se creó el muro del usuario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() 
     {
-        dd(auth()->user());
+        return view('dashboard');
     }
 
 }
