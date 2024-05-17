f86eb29165e3934c7c4348eafece1c04518e2f9b
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-17 16:46:01 -0600

    Se implementó la forma de mostrar el nombre del usuario dinámicamente

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,9 @@ class PostController extends Controller
 
     public function index(User $user) 
     {
-        return view('dashboard');
+        return view('dashboard', [
+            'user' => $user
+        ]);
     }
 
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <img src="{{ asset('img/usuario.svg') }}" alt="imagen usuario" />
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5">
-                <p class="text-gray-700 text-2xl">{{ auth()->user()->username }}</p>
+                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
             </div>
         </div>
    </div>
