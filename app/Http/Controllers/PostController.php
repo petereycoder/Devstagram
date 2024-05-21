ef0d51b7521a1cda753b88779c66bd0909fcc0cc
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-21 15:48:38 -0600

    Se implemtó la forma para almacenar publicaciones

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -30,6 +31,15 @@ class PostController extends Controller
             'titulo' => 'required|max:255',
             'descripcion' => 'required'
         ]);
+
+        Post::create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 
 }
