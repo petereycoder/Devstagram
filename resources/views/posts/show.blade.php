5d42cb29fc3d892f66f990386665c464c93e12af
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-28 10:40:13 -0600

    Se implementó la funcionalidad de mostrar la cantidad de likes en la publicación

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -35,7 +35,9 @@
                         </form>
                      @endif
                 @endauth
-                <p>0 likes</p>
+                <p class="font-bold">{{ $post->likes->count() }}
+                    <span class="font-normal">Likes</span>
+                </p>
             </div>
             <div>
                 <p class="font-bold">{{ $post->user->username }}</p>
