ddc2010c330ef71855615232f9b99f09f4aeb064
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-29 19:31:26 -0600

    Se almacenó la información del usuario y creó la migración para añadir imágenes al perfil del usuario

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class PerfilController extends Controller
 {
@@ -26,5 +29,30 @@ class PerfilController extends Controller
        $this->validate($request, [
         'username' => ['required','unique:users,username,'.auth()->user()->id,'min:3','max:20','not_in:twitter,editar-perfil']
        ]);
+
+       if($request->imagen){
+            $imagen = $request->file('imagen');
+ 
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+ 
+            $manager = new ImageManager(new Driver());
+ 
+            $imagenServidor = $manager::gd()->read($imagen);
+            $imagenServidor->cover(1000,1000);
+ 
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+ 
+            $imagenServidor->save($imagenPath);
+       }
+
+       //Guardar cambios
+       $usuario = User::find(auth()->user()->id);
+       $usuario->username = $request->username;
+       $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? null;
+       $usuario->save();
+
+       //Redireccionar
+       return redirect()->route('posts.index', $usuario->username);       
+
     }
 }
