2e68b9a85ca239065cc0b999a247b8b27061aeba
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-29 18:30:14 -0600

    Se creó y validó el formulario

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PerfilController extends Controller
@@ -15,5 +16,15 @@ class PerfilController extends Controller
     public function index()
     {
         return view('perfil.index');
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+       $this->validate($request, [
+        'username' => ['required','unique:users,username,'.auth()->user()->id,'min:3','max:20','not_in:twitter,editar-perfil']
+       ]);
     }
 }
