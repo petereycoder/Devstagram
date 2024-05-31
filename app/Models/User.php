6ee64c9f97bc749c8edfc85ffc23b4f2160b9bfc
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-30 18:35:33 -0600

    Se implementa de mostrar formulario de forma condicional

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,10 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
     }
+
+    //Comprobar si un usuario ya sigue a otro
+    public function siguiendo(User $user)
+    {
+        return $this->followers()->contains($user->id);
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,29 +45,32 @@
                 </p>
                 @auth
                     @if ($user->id !== auth()->user()->id)
-                        <form 
-                            action="{{ route('users.follow', $user) }}"
-                            method="POST"
-                        >
-                            @csrf
-                            <input 
-                                type="submit"
-                                class="bg-blue-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
-                                value="Seguir"
-                            />
-                        </form>
-                        <form 
-                            action="{{ route('users.unfollow', $user) }}"
-                            method="POST"
-                        >
-                            @method('DELETE')
-                            @csrf
-                            <input 
-                                type="submit"
-                                class="bg-red-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
-                                value="Dejar de Seguir"
-                            />
-                        </form>
+                        @if ($user->siguiendo(auth()->user()))
+                            <form 
+                                action="{{ route('users.follow', $user) }}"
+                                method="POST"
+                            >
+                                @csrf
+                                <input 
+                                    type="submit"
+                                    class="bg-blue-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
+                                    value="Seguir"
+                                />
+                            </form>
+                        @else
+                            <form 
+                                action="{{ route('users.unfollow', $user) }}"
+                                method="POST"
+                            >
+                                @method('DELETE')
+                                @csrf
+                                <input 
+                                    type="submit"
+                                    class="bg-red-600 text-white uppercase rounded-lg px-3 py-1 text-xs font-bold cursor-pointer"
+                                    value="Dejar de Seguir"
+                                 />
+                            </form>
+                        @endif
                     @endif
                 @endauth
             </div>
