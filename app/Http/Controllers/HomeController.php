e04760587033f8ff22a42fa7b18fb449562abe05
petereycoder <fp.pipe.reyes@gmail.com>
2024-05-31 18:53:41 -0600

    Se obtiene los usuarios que seguimos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,8 @@ class HomeController extends Controller
 {
     public function __invoke()
     {
+        //Obtener a quienes seguimos
+        dd(auth()->user()->followings->pluck('id')->toArray());
         return view('home');
     }
 }
