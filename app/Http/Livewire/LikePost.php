8b0adbeebcd078de8988b7359f954a1a6caad2a7
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-03 13:45:48 -0600

    Se implementó interfaces reactivas con livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -7,15 +7,23 @@ use Livewire\Component;
 class LikePost extends Component
 {
     public $post;
+    public $isliked;
+
+    public function mount($post)
+    {
+        $this->isliked = $post->checkLike(auth()->user());
+    }
 
     public function like(){
         if( $this->post->checkLike(auth()->user() ))
         {
             $this->post->likes()->where('post_id', $this->post->id)->delete();
+            $this->isliked = false;
         }else{
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
+            $this->isliked = true;
         }
     }
 
