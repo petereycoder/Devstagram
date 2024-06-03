7c65986f79f9f35eca3724a352c987828a5a9fc4
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-03 12:08:04 -0600

    Se instaló y se creó un componente de livewire

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,6 +10,7 @@
             <img src="{{ asset('uploads') . "/" . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
             <div class="p-3 flex items-center gap-4">
                 @auth
+                    <livewire:like-post />
                     @if ($post->checkLike(auth()->user()))
                         <form method="POST" action="{{ route('posts.like.destroy', $post) }}">
                             @method('DELETE')
