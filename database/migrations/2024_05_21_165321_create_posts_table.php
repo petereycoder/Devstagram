1eebf944b736ddb8f4f41037c677c983e7239b66
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-04 10:12:03 -0600

    Se dejaron visibles las carpetas para guardar las imágenes

--- database/migrations/2024_05_21_165321_create_posts_table.php
+++ database/migrations/2024_05_21_165321_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('titulo');
             $table->text('descripcion');
-            $table->string('imagen')->nullable();;
+            $table->string('imagen');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
